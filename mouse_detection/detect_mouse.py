7587f428a46f3e6fb769cab0ac306ce6af8d7646
Ariel <ariel.iporre.rivas@gmail.com>
2021-04-27 17:17:38 +0200

    reading video function

--- mouse_detection/detect_mouse.py
+++ mouse_detection/detect_mouse.py
@@ -1,1 +1,35 @@
 import cv2
+import numpy as np
+
+
+def read_video(video_path, block=False, num_blocks=None, index=None):
+    print('Reading video: ', video_path)
+    cap = cv2.VideoCapture(video_path)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print('video props: (frameCount, frameHeight, frameWidth)=', (frameCount, frameHeight, frameWidth))
+    fc = 0
+    ret = True
+    if not block:
+        buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+        while (fc < frameCount and ret):
+            ret, buf[fc] = cap.read()
+            fc += 1
+        cap.release()
+    else:
+        # calculate block indices:
+        block_length = frameCount // num_blocks
+        a = index * block_length
+        b = a + block_length
+        if index == num_blocks - 1:
+            b += frameCount % num_blocks
+        buf = np.empty((b - a, frameHeight, frameWidth, 3), np.dtype('uint8'))
+        cnt = 0
+        while (fc < frameCount and ret and fc < b):
+            ret, frame = cap.read()
+            if fc < b and fc >= a:
+                buf[cnt] = frame
+                cnt += 1
+            fc += 1
+    return buf
